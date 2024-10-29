09e596608663fa494bee5f79d1985e85e675b2cc
CubedWolf <william.bergeron@outlook.com>
2024-10-29 05:40:16 +0000

    idk atp

--- Python/WoahBilly.py
+++ Python/WoahBilly.py
@@ -1,1 +1,8 @@
 print("HI")
+
+git init
+git add .
+git commit -m "my commit"
+git remote set-url origin git@github.com:username/repo.git
+git remote https://github.com/CubdWolf/BillyProjects@github.com:CubedWolf/SillyBilly
+git push origin main
